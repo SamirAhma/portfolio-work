c850beeac2169d82dbecc5c55e3152ef83c13c0e
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2023-01-11 00:15:30 +0800

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <script
+          async
+          src="https://identity.netlify.com/v1/netlify-   identity-widget.js"
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
