adac4daefe234d9d2bc33bc1416e63f4557019d1
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2023-01-13 13:28:43 +0800

    change something

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -13,42 +13,50 @@ const PostBody = ({ content, githubLink, demoLink }: Props) => {
   return (
     <div className="max-w-4xl mx-auto">
       <div className="flex">
-        <a
-          href={demoLink}
-          className="flex px-4 py-2 font-medium text-white bg-green-500 rounded-lg hover:bg-green-600"
-          rel="noopener noreferrer"
-        >
-          <div className="flex items-center justify-center mr-2">
-            <Image src={desktop} width={20} height={20} alt="new tab" />
-          </div>
-          Demo
-        </a>
-        <a
-          href={demoLink}
-          className="flex px-4 py-2 mx-4 font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="flex items-center justify-center mr-2">
-            <Image src={external} width={20} height={20} alt="new tab" />
-          </div>
-          Open Demo in new tab
-        </a>
-        <a
-          href={githubLink}
-          className="flex px-4 py-2 font-medium text-white bg-black rounded-lg hover:bg-gray-700"
-        >
-          <div className="flex items-center justify-center mr-2">
-            <Image
-              src={github}
-              width={20}
-              height={20}
-              color="white"
-              alt="github"
-            />
-          </div>
-          GitHub
-        </a>
+        {demoLink && (
+          <a
+            href={demoLink}
+            className="flex px-4 py-2 mr-4 font-medium text-white bg-green-500 rounded-lg hover:bg-green-600"
+            rel="noopener noreferrer"
+          >
+            <div className="flex items-center justify-center mr-2">
+              <Image src={desktop} width={20} height={20} alt="new tab" />
+            </div>
+            Demo
+          </a>
+        )}
+        {demoLink && (
+          <a
+            href={demoLink}
+            className="flex px-4 py-2 mr-4 font-medium text-white bg-blue-500 rounded-lg hover:bg-blue-600"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="flex items-center justify-center mr-2">
+              <Image src={external} width={20} height={20} alt="new tab" />
+            </div>
+            Open Demo in new tab
+          </a>
+        )}
+        {demoLink && (
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={githubLink}
+            className="flex px-4 py-2 font-medium text-white bg-black rounded-lg hover:bg-gray-700"
+          >
+            <div className="flex items-center justify-center mr-2">
+              <Image
+                src={github}
+                width={20}
+                height={20}
+                color="white"
+                alt="github"
+              />
+            </div>
+            GitHub
+          </a>
+        )}
       </div>
       <div
         className={`${markdownStyles["markdown"]}`}
