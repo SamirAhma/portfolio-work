c63d3112336062a7c898fead20adc320abe29a8c
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2023-01-14 15:15:01 +0800

    delete react wait

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,6 @@
 import { AppProps } from "next/app";
 import "../styles/index.css";
-import { Waiter } from "react-wait";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Waiter>
-      <Component {...pageProps} />
-    </Waiter>
-  );
+  return <Component {...pageProps} />;
 }
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -3,9 +3,6 @@ import Layout from "../../components/layout";
 import { getPostBySlug, getAllPosts } from "../../lib/api";
 import markdownToHtml from "../../lib/markdownToHtml";
 import type PostType from "../../interfaces/post";
-import { useState } from "react";
-import { Loading } from "../../components/Loading";
-import { useEffect } from "react";
 import PostHeader from "../../components/post-header";
 import { getPlaiceholder } from "plaiceholder";
 import LazyLoad from "react-lazy-load";
@@ -16,48 +13,29 @@ type Props = {
   preview?: boolean;
 };
 
-import { useWait } from "react-wait";
-
 export default function Post({ post }: Props) {
-  const [loading, setLoading] = useState(true);
-  const { startWaiting, endWaiting, Wait } = useWait();
-  useEffect(() => {
-    startWaiting("fullPageLoad");
-    // set a timeout for the loading message to be displayed for a certain period of time
-    setTimeout(() => {
-      endWaiting("fullPageLoad");
-      setLoading(false);
-    }, 1100);
-  }, []);
-
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
-    <Wait on="fullPageLoad">
-      <Layout>
-        <Container>
-          <article className="mb-16">
-            <PostHeader
-              title={post.title}
-              coverImage={post.coverImage}
-              base64={post.base64}
-            />
-            <PostBody
-              content={post.content}
-              githubLink={post.linkGithub}
-              demoLink={post.linkDemo}
-            />
-          </article>
-          <div className="max-w-4xl mx-auto mt-2 mb-16">
-            <LazyLoad height={900}>
-              <Githubintro />
-            </LazyLoad>
-          </div>
-        </Container>
-      </Layout>
-    </Wait>
+    <Layout>
+      <Container>
+        <article className="mb-16">
+          <PostHeader
+            title={post.title}
+            coverImage={post.coverImage}
+            base64={post.base64}
+          />
+          <PostBody
+            content={post.content}
+            githubLink={post.linkGithub}
+            demoLink={post.linkDemo}
+          />
+        </article>
+        <div className="max-w-4xl mx-auto mt-2 mb-16">
+          <LazyLoad height={900}>
+            <Githubintro />
+          </LazyLoad>
+        </div>
+      </Container>
+    </Layout>
   );
 }
 
